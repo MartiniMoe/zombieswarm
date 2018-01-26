8e65c20295757b8ffc22771300605f4f18024ff3
MartiniMoe <martini.moe@cheex.de>
2018-01-26 21:54:06 +0100

    make pedestrian look in direction of movement

--- godot_project/pedestrian/Pedestrian.gd
+++ godot_project/pedestrian/Pedestrian.gd
@@ -1,7 +1,11 @@
 extends KinematicBody2D
 
 const MOTION_SPEED = 160
-var MOVEMENT_RANGE = 200
+const MOVEMENT_RANGE = 200
+const spr_up = preload("res://pedestrian/pedestrian-up.png")
+const spr_down = preload("res://pedestrian/pedestrian-down.png")
+const spr_left = preload("res://pedestrian/pedestrian-left.png")
+const spr_right = preload("res://pedestrian/pedestrian-right.png")
 
 var moving = false
 var movement_start
@@ -15,12 +19,20 @@ func _physics_process(delta):
 		if randi()%200 == 0:
 			moving = true
 			movement_relative = Vector2(randi()%MOVEMENT_RANGE-MOVEMENT_RANGE/2, randi()%MOVEMENT_RANGE-MOVEMENT_RANGE/2)
-			print("moving, relative is " + str(movement_relative))
 			movement_start = get_global_position()
+			if abs(movement_relative.y) > abs(movement_relative.x):
+				if movement_relative.y > 0:
+					$Sprite.texture = spr_down
+				else:
+					$Sprite.texture = spr_up
+			else:
+				if movement_relative.x > 0:
+					$Sprite.texture = spr_right
+				else:
+					$Sprite.texture = spr_left
 	else:
 		var distance = get_global_position().distance_to(movement_start + movement_relative)
 		if distance >= 2:
 			move_and_slide(movement_relative.normalized() * MOTION_SPEED)
 		else:
-			print("stopping")
 			moving = false
