596de614eb6803b3e9857dfd1df82a440ee19543
MartiniMoe <martini.moe@cheex.de>
2018-01-27 02:04:51 +0100

    add title screen

--- godot_project/menu/Menu.gd
+++ godot_project/menu/Menu.gd
@@ -3,5 +3,8 @@ extends Control
 func _ready():
 	randomize()
 
-func _on_Start_pressed():
+func _on_StartButton_pressed():
 	get_tree().change_scene("res://level01/Level01.tscn")
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
